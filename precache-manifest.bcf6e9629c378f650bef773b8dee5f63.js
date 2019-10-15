5ea87e0edaa0d60076f317a3e52997bc0b083de8
Muhammad_Bilal <bilalaarbi300@gmail.com>
2019-10-14 18:53:51 -0700

    Updates

---
+++ precache-manifest.bcf6e9629c378f650bef773b8dee5f63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f1431976d651ebd41ed9b2a87cc358",
+    "url": "/react-list-app-with-redux/index.html"
+  },
+  {
+    "revision": "cdae50ede83871161e3d",
+    "url": "/react-list-app-with-redux/static/css/main.909e186c.chunk.css"
+  },
+  {
+    "revision": "b1f521215453e7b8329a",
+    "url": "/react-list-app-with-redux/static/js/2.522779d8.chunk.js"
+  },
+  {
+    "revision": "cdae50ede83871161e3d",
+    "url": "/react-list-app-with-redux/static/js/main.e8c8eac4.chunk.js"
+  },
+  {
+    "revision": "9a86a2605914d49bca43",
+    "url": "/react-list-app-with-redux/static/js/runtime~main.67d34e49.js"
+  }
+]);
